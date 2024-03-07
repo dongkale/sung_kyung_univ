b9a7ab71403c72cac55426b66ea58388b0f08224
LENNON\dklee <dongkale@naver.com>
2024-03-07 15:26:12 +0900

    누락 화일 추가

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ const mix = require("laravel-mix");
 
 mix.js("resources/js/app.js", "public/js")
     .copyDirectory("resources/js", "public/js")
+    .copyDirectory("resources/images", "public/images")
     .postCss("resources/css/app.css", "public/css", [
         //
     ])
